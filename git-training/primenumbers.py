c3af9ba42bd92e1b37af2c0bafcf5a0591d62861
Lawrence Wachira <larrywachira@gmail.com>
2017-01-18 01:56:17 +0300

    Fixed bug in function that generated only two prime numbers

--- git-training/primenumbers.py
+++ git-training/primenumbers.py
@@ -1,26 +1,27 @@
 # A function that generates prime numbers from 0 to n
 
-import big_o
+#import big_o
 
+
+def is_prime(x):
+    if x % 2 != 0 and x % 3 != 0 and x % 5 != 0:
+        return True
 
 def prime_numbers(n):
+
     print('2\n3')
 
-    def is_prime(x):
-        if x % 2 != 0 and x % 3 != 0 and x % 5 != 0:
-            return True
-
     for i in range(5, n, 2):
-        if is_prime(n):
+        if is_prime(i):
             print(i)
 
-print(prime_numbers(n))
+print(prime_numbers(100))
 
 # Asymptotic analysis
 
-positive_int_generator = lambda n: big_o.datagen.n_(10000)
-best, others = big_o.big_o(prime_numbers, positive_int_generator, n_repeats=100)
+#positive_int_generator = lambda n: big_o.datagen.n_(10000)
+#best, others = big_o.big_o(prime_numbers, positive_int_generator, n_repeats=100)
 
-print(best)
+#print(best)
 
 
