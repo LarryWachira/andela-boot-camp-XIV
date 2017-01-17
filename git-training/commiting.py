7daaaa47a67671b77a60ed513d1329067052a412
Lawrence Wachira <larrywachira@gmail.com>
2017-01-17 10:53:57 +0300

    Added a multiplication function

---
+++ git-training/commiting.py
@@ -0,0 +1,9 @@
+print('This is a calculator that multiplies two numbers.\n')
+a = int(input('Input your first number: \n>'))
+b = int(input('Input the second number: \n>'))
+
+def multiply(a,b):
+    return a*b
+
+print("The answer is: ", multiply(a,b))
+
