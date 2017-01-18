17f433d95f2db8f63d0f1497407add1e2d95aa29
Lawrence Wachira <larrywachira@gmail.com>
2017-01-18 21:10:45 +0300

    How many humans are in space? Version 1.0 - bug fixes

--- day-2/cl-app.py
+++ day-2/cl-app.py
@@ -1,7 +1,7 @@
 import requests
 
-response = requests.get("http://api.open-notify.org/arson.json")
-info = response.json
+response = requests.get("http://api.open-notify.org/astros.json")
+info = response.json()
 
 if response.status_code == 200:
     print("Want to play 'Guess how many people are in space'?\nType Y or N:")
@@ -11,15 +11,15 @@ if response.status_code == 200:
         print("Well, what are you waiting for? Guess how many humans are in space right now:\n> ")
         answer = input()
 
-        if answer == info["number"]:
+        if int(answer) == info["number"]:
             print("Of course. Google, right? \n\n:( ")
         else:
             print("Wrong! There are {} people in space right now.".format(info["number"]))
-            print("\n That'll be one billion dollars. I'll take cash.")
+            print("\nThat'll be one billion dollars. I'll take cash.")
 
     elif answer == 'N' or answer == 'n':
-        print("You must be a lot of fun at parties. \n\n:(")
+        print("\nYou must be a lot of fun at parties. \n:(")
 
-    else: print("Seriously?")
+    else: print("\nSeriously? And the darwin award goes to...")
 
 else: print("Error: {}\n\nPlease try again later.".format(response.status_code))
