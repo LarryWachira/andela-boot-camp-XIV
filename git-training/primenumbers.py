bffa39a3157c459de871666747ced13a085f599b
Lawrence Wachira <larrywachira@gmail.com>
2017-01-18 22:53:11 +0300

    Added negative number condition

--- git-training/primenumbers.py
+++ git-training/primenumbers.py
@@ -8,24 +8,26 @@ def is_prime(x):
         return True
 
 def prime_numbers(n):
-
-    if n is 1: print('one is special')
-    elif n is 2: print("2")
-    elif n is 3: print('2\n3')
-    elif n is 5: print("2\n3\n5")
+    if n > 1:
+        if n is 1: print('one is special')
+        elif n is 2: print("2")
+        elif n is 3: print('2\n3')
+        elif n is 5: print("2\n3\n5")
+        else:
+            print('2\n3\n5')
+            for i in range(7, n+1, 2):
+                if is_prime(i):
+                    print(i)
     else:
-        print('2\n3\n5')
-        for i in range(7, n+1, 2):
-            if is_prime(i):
-                print(i)
+        return False
 
 print(prime_numbers(100))
 
 # Asymptotic analysis
 
-#positive_int_generator = lambda n: big_o.datagen.n_(10000)
-#best, others = big_o.big_o(prime_numbers, positive_int_generator, n_repeats=100)
+'''positive_int_generator = lambda n: big_o.datagen.n_(10000)
+best, others = big_o.big_o(prime_numbers, positive_int_generator, n_repeats=100)
 
-#print(best)
+print(best)'''
 
 
