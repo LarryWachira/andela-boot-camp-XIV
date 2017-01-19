d9ca1e829447658024ddb99b7e2ba24e3c3c7d45
Lawrence Wachira <larrywachira@gmail.com>
2017-01-19 12:05:30 +0300

    Bug fixes

--- day-2/command-line-application/cl-app.py
+++ day-2/command-line-application/cl-app.py
@@ -4,17 +4,21 @@ response = requests.get("http://api.open-notify.org/astros.json")
 info = response.json()
 
 if response.status_code == 200:
-    print("Want to play 'Guess how many people are in space'?\nType Y or N:")
-    answer = input('> ')
+    print("\nWant to play 'Guess how many people are in space'?\n\n")
+    answer = input('Type Y or N:> ')
 
     if answer == 'Y' or answer == 'yes' or answer =='y' or answer == 'YES' or answer == 'Yes':
-        print("Well, what are you waiting for? Guess how many humans are in space right now:")
+        print("\nWell, what are you waiting for? Guess how many humans are in space right now:")
         answer = input('\n> ')
 
-        if int(answer) == info["number"]:
-            print("\nOf course. Google, right? \n\n:( ")
-        else:
-            print("Wrong! There are {} people in space right now.".format(info["number"]))
+        try:
+            if int(answer) == info["number"]:
+                print("\nOf course. Google, right? \n\n:( ")
+            else:
+                print("\nWrong! There are {} people in space right now.".format(info["number"]))
+                print("\nThat'll be one billion dollars. I'll take cash.")
+        except:
+            print("\nWrong! There are {} people in space right now.".format(info["number"]))
             print("\nThat'll be one billion dollars. I'll take cash.")
 
     elif answer == 'N' or answer == 'NO' or answer == 'n' or answer == 'no' or answer == 'No':
